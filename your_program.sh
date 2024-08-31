c7bcf91412f6fb1805690c1398f75cc4d27f5896
Cook4104 <bastian4506@gmail.com>
2024-08-31 16:54:44 +0800

    test fix run.sh

--- your_program.sh
+++ your_program.sh
@@ -6,7 +6,7 @@
 #
 # Learn more: https://codecrafters.io/program-interface
 
-# set -e # Exit early if any commands fail
+set -e # Exit early if any commands fail
 
 # Copied from .codecrafters/compile.sh
 #
@@ -14,7 +14,7 @@
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+  cmake -B build . -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
   cmake --build ./build
 )
 
