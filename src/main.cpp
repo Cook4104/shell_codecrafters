2b1cb042a8430ae7f6c667b075e802f3639b15b2
Cook4104 <bastian4506@gmail.com>
2024-08-31 17:48:58 +0800

    code go brrrt

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,49 @@
+#include <cstdlib>
 #include <iostream>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+std::vector<std::string> SplitString(std::string pString,char delim){
+  std::vector<std::string> strings;
+  std::istringstream f(pString);
+  std::string cString;
+  while(std::getline(f,cString,delim)){
+    strings.push_back(cString);
+  }
+  return strings;
+}
+
+
+typedef void (*Command)(std::vector<std::string> args);
+
+void ExitCommand(std::vector<std::string> args){
+  exit(0);
+}
+
+void Nothing(std::vector<std::string> args){}
+
+std::unordered_map<std::string, Command> commands{
+  {"exit",ExitCommand}
+};
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;  
 
-  bool isAlive = true;
-
-  while(isAlive){
+  while(1){
     std::cout << "$ ";
 
     std::string input;
     std::getline(std::cin, input);
- 
-    std::cout << input << ": command not found" << std::endl;
+    std::vector<std::string> cmd = SplitString(input,' ');
+    if(cmd.empty()) continue; 
+    if(commands.find(cmd[0]) != commands.end()){
+      commands[cmd[0]](cmd);
+    }else{
+      std::cout << input << ": command not found" << std::endl;
+    }
   }
 }
